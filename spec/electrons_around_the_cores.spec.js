1d643fb68ade7ad51460f012f69a6d04ca865763
jakuslizy <jakuslizy@gmail.com>
2024-05-21 22:20:07 +0200

    Aliz: update todo and inital specs

--- spec/electrons_around_the_cores.spec.js
+++ spec/electrons_around_the_cores.spec.js
@@ -1,12 +1,11 @@
 describe('Electrons around the cores', function () {
- var electrons = new Electrons;
+    var electrons = new Electrons;
 
     it('it should return nothing for an input type string', function () {
-        expect(electrons.calculate('')).toEqual();
+        expect(electrons.calculate('')).toEqual(undefined);
     });
 
     it('it should return nothing for an input of an empty array', function () {
-        expect(electrons.calculate([])).toEqual();
+        expect(electrons.calculate([])).toEqual(undefined);
     });
-
 });
--- src/electrons_around_the_cores.js
+++ src/electrons_around_the_cores.js
@@ -1,6 +1,5 @@
 function Electrons() {}
+
 Electrons.prototype.calculate = function (electrons) {
     
-    if(typeof electrons === String) return;
-    if(electrons.length === 0) return;
 };
