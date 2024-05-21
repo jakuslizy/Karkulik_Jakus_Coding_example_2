7ca291ae3189ad409829caf4379eb6575384fc06
jakuslizy <jakuslizy@gmail.com>
2024-05-21 21:23:21 +0200

    Aliz: Failing first test

---
+++ spec/electrons_around_the_cores.spec.js
@@ -0,0 +1,7 @@
+describe('Electrons around the cores', function () {
+ var electrons = new Electrons;
+
+    it('it should return nothing for an input type string', function () {
+        expect(electrons.calculate('')).toEqual("");
+    });
+});
