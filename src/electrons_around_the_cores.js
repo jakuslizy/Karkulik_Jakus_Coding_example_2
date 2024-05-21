463b317239937519e472cf662be3f2c8677c36a1
pedaKrk <peter.karkulik@gmail.com>
2024-05-21 21:41:08 +0200

    peter: production code for testcase 2

--- src/electrons_around_the_cores.js
+++ src/electrons_around_the_cores.js
@@ -1,6 +1,6 @@
 function Electrons() {}
 Electrons.prototype.calculate = function (electrons) {
     
-    if(typeof electrons === String)
-        return;
+    if(typeof electrons === String) return;
+    if(electrons.length === 0) return;
 };
