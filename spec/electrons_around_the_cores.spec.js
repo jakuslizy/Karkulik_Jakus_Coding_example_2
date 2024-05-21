35ba3a0f78838531e5c3b6ea821c6035b4151c24
pedaKrk <peter.karkulik@gmail.com>
2024-05-21 21:35:06 +0200

    peter: first test case

--- spec/electrons_around_the_cores.spec.js
+++ spec/electrons_around_the_cores.spec.js
@@ -2,6 +2,6 @@ describe('Electrons around the cores', function () {
  var electrons = new Electrons;
 
     it('it should return nothing for an input type string', function () {
-        expect(electrons.calculate('')).toEqual("");
+        expect(electrons.calculate('')).toEqual();
     });
 });
